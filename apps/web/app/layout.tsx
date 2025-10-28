15b2f941f0a441114c638a739f79fe17482af8f8
João Eduardo Amorim Jardim <joao.jardim@acertapromotora.com.br>
2025-10-28 13:30:11 -0300

    feat: adds some minor improviments

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import './styles.css'
 
 export default function RootLayout({
   children
---
+++ packages/eslint-config/next.d.ts
@@ -0,0 +1,4 @@
+declare module '@repo/eslint-config/next-js' {
+  const config: any;
+  export default config;
+}
