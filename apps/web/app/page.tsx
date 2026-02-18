15449dea6966969c21a7e7121ef176a05e280ff9
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2026-02-18 03:52:56 -0300

    feat: create new tokens package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from '../components'
-
 export default function Web() {
   return (
     <div>
-      <div className="bg-background-fill-accent-primary m-10 border-8 border-amber-900 p-10 shadow-2xl">
-        teste tailwind
-      </div>
-      <Button>teste 123</Button>
+      <div>teste tailwind</div>
     </div>
   )
 }
