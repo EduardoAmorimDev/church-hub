80253334231ea78166bde2222fe0e46a38d09990
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2026-02-26 00:51:43 -0300

    fix: update font-family global in app and storybook

--- apps/web/.storybook/preview.tsx
+++ apps/web/.storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/nextjs'
+import 'material-symbols'
 import '../app/styles.css'
 import '../models/enums'
 import { ThemeDecorator } from './decorators'
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button, Icon } from '~/components'
+
 export default function Web() {
   return (
-    <div>
-      <button className="bg-red-50">Teste colors</button>
+    <div className="flex h-screen items-center justify-center">
+      <Button color="destructive" size="medium" variant="filled">
+        Button text
+        <Icon name="brightness_1" />
+      </Button>
+
+      <Icon name="brightness_1" weight={400} />
     </div>
   )
 }
